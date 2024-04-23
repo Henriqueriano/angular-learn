166d293992341be05b2aaa75f2459e908d496737
Daniel Henrique <daniel.neves0021@gmail.com>
2024-04-23 11:50:50 -0300

    f commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PageComponent } from '../page/page/page.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'page',
+        component: PageComponent
+    },
+    {
+        path: '',
+        component: AppComponent
+    }];
